57e68e0c47d20ddea9413d1da55d55dcd92d6f96
Mikolaj Speichert <mikolaj.speichert@tooploox.com>
2019-03-20 11:14:49 +0100

    Fix problem with config dependency injection

--- server/app.ts
+++ server/app.ts
@@ -136,8 +136,9 @@ app.get("/api/history", (req, res) => {
         .then(j => res.json(j))
 })
 
-export const onSendEmail: (cfg: gitfs.Config) => express.RequestHandler = cfg =>
+export const onSendEmail: (cfg?: gitfs.Config) => express.RequestHandler = cfg =>
     async (req, res) => {
+        if(!cfg) cfg = gitfs.config
         const msg = req.body as Message
         const allowed = (cfg.allowedEmailRecipients || []).find(o => o === req.body.to)
         winston.info("api-send allowed: " + allowed + " " + JSON.stringify(msg))
@@ -162,7 +163,7 @@ const limiter = new RateLimit({
 });
 
 app.use("/api/send-email", limiter)
-app.post("/api/send-email", onSendEmail(gitfs.config))
+app.post("/api/send-email", onSendEmail())
 
 app.post("/api/uploadimg", (req, res) => {
     if (!req.appuser)
