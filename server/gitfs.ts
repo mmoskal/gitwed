3c93e498116710f4beb8f2ab761650569fb45238
Mikolaj Speichert <mikolaj.speichert@tooploox.com>
2019-04-01 13:03:10 +0200

    Add custom redirect path

--- server/gitfs.ts
+++ server/gitfs.ts
@@ -28,6 +28,7 @@ export interface Config {
     vhosts?: SMap<string>;
     certEmail?: string;
     defaultRedirect?: string; // defaults to /events/
+    custom404Redirect?: string;
     allowedEmailRecipients?: string[];
     services?: rest.ServiceConfig[];
 }
